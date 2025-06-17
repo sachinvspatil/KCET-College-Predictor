78db0c88a30a789071085e6596e712fe016c7883
sachinvspatil <41478412+sachinvspatil@users.noreply.github.com>
2025-06-17 21:21:54 +0530

    Update kcet_predictor.py

--- pages/kcet_predictor.py
+++ pages/kcet_predictor.py
@@ -1,7 +1,1 @@
-import streamlit as st
-import pandas as pd
-
-# ------------------ Config ------------------
-#st.set_page_config(page_title="KCET College Predictor 2025", page_icon="🎓", layout="centered")
-#st.title("🎓 KCET 2024 College Predictor")
 
